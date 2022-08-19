4494166354a5a02fee1698e50cfafdeba1bc0488
battlebot2 <111550844+trevorthebike@users.noreply.github.com>
2022-08-19 02:58:56 +0000

    Added trapped

--- index.js
+++ index.js
@@ -1,28 +1,27 @@
 
 let myhttp = require("http");
 
-let myserver = myhttp.createServer( 
-    function( myrequest, myresponse ) {
-        console.log( myrequest.url );
+let myserver = myhttp.createServer(
+	function(myrequest, myresponse) {
+		console.log(myrequest.url);
 
-        let mytext;
-        if ( myrequest.url === "/hey" ) {
-        mytext = "Well hello there...";
-        } else {
-        mytext = "I don't know you!";
-        }
+		let mytext;
+		if (myrequest.url === "/hey") {
+			mytext = "Well hello there...";
+		} else {
+			mytext = "I don't know you!";
+		}
 
-        mytext = mytext + " - Please help me... I am trapped in a Node.js server!";
+		mytext = mytext + " - Please help me... I am trapped in a Node.js server! So trapped";
 
-        // writeHead() creates an http response header, including the status code (200 OK), the content type
-        myresponse.writeHead( 200, { "Content-Type": "text/plain" } );
-        myresponse.end( mytext + "\n" );
-    }
+		// writeHead() creates an http response header, including the status code (200 OK), the content type
+		myresponse.writeHead(200, { "Content-Type": "text/plain" });
+		myresponse.end(mytext + "\n");
+	}
 
- );
+);
 
 // ask http to start listening on a tcp port for incoming http requests
 // listen() takes 2 args: 1: tcp port #, string of the ip address to listen (0.0.0.0)
 myserver.listen(8080, "0.0.0.0");
-
 
